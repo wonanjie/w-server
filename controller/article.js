8cf35a912530bd7b38bdc39ec1299b0e8fc64e25
wonanjie <992239492@qq.com>
2020-05-27 17:13:42 +0800

    新增删除文章接口 更新文章接口

--- controller/article.js
+++ controller/article.js
@@ -3,41 +3,49 @@
  * @Author: wonanjie
  * @Date: 2020-05-25 14:58:41
  * @LastEditors: wonanjie
- * @LastEditTime: 2020-05-27 13:05:13
- */ 
+ * @LastEditTime: 2020-05-27 15:18:29
+ */
 
-const {Tool}=require('../model/tool')
+const { Tool } = require('../model/tool')
 const { ErrorModel, SuccessModel } = require('../model/resModel')
-const {exec}=require('../db/mysql')
-let tool=new Tool()
-const newArticle=async (body)=>{
-    if(tool.isEmpty(body)) return new ErrorModel('新增文章失败')
-    if(tool.isEmpty(body.author)) return new ErrorModel('作者不存在')
-    if(tool.isEmpty(body.title)) return new ErrorModel('请填写标题标题')
-    if(tool.isEmpty(body.content)) return new ErrorModel('请填写内容')
-    const author=body.author
-    const title=body.title
-    const content=body.content
-    const createTime=Date.now()
-    const sql=`insert into article(author,title,content,createTime) values('${author}','${title}','${content}','${createTime}');`
+const { exec } = require('../db/mysql')
+let tool = new Tool()
+const newArticle = async (body) => {
+    if (tool.isEmpty(body) || tool.isEmpty(body.author) || tool.isEmpty(body.title) || tool.isEmpty(body.content)) return new ErrorModel('新增文章失败')
+    const createTime = Date.now()
+    const sql = `insert into article(author,title,content,createTime) values('${body.author}','${body.title}','${body.content}','${createTime}');`
     const data = await exec(sql)
     console.log(data.insertId)
     return new SuccessModel('新增文章成功')
 }
 
-const getArticleList= async (page)=>{
-    if(tool.isEmpty(page)) return new ErrorModel('页码不存在') 
-    const sql=`select * from article limit ${(page-1)*10}, 10;`
+const getArticleList = async (page) => {
+    if (tool.isEmpty(page)) return new ErrorModel('获取文章列表失败')
+    const sql = `select * from article limit ${(page - 1) * 10}, 10;`
     const data = await exec(sql)
-    return new SuccessModel(data,'获取文章列表成功') 
+    return new SuccessModel(data, '获取文章列表成功')
 }
 
-const getArticleDetail=async (id)=>{
-    if(tool.isEmpty(id)) return new ErrorModel('文章id不存在') 
-    const sql=`select * from article where id='${id}' `
+const getArticleDetail = async (id) => {
+    if (tool.isEmpty(id)) return new ErrorModel('获取文章详情失败')
+    const sql = `select * from article where id='${id}';`
     const data = await exec(sql)
-    return new SuccessModel(data,'获取文章详情成功') 
+    return new SuccessModel(data, '获取文章详情成功')
 }
-module.exports={
-    newArticle,getArticleList,getArticleDetail
+
+const deleteArticle = async (body) => {
+    if (tool.isEmpty(body)||tool.isEmpty(body.id)) return new ErrorModel('删除文章失败')
+    const sql = `delete from article where id='${body.id}';`
+    const data = await exec(sql)
+    return new SuccessModel('删除文章成功')
+}
+
+const updateArticle=async (body)=>{
+    if (tool.isEmpty(body)||tool.isEmpty(body.id)||tool.isEmpty(body.title)||tool.isEmpty(body.content)) return new ErrorModel('修改文章失败')
+    const sql=`update article set title='${body.title}', content='${body.content}' where id=${body.id}`
+    const data = await exec(sql)
+    return new SuccessModel('更新文章成功')
+}
+module.exports = {
+    newArticle, getArticleList, getArticleDetail, deleteArticle,updateArticle
 }
--- routes/article.js
+++ routes/article.js
@@ -3,11 +3,11 @@
  * @Author: wonanjie
  * @Date: 2020-05-25 14:57:28
  * @LastEditors: wonanjie
- * @LastEditTime: 2020-05-27 13:07:34
+ * @LastEditTime: 2020-05-27 15:08:43
  */ 
 const router = require('koa-router')()
 
-const {newArticle,getArticleList,getArticleDetail}=require('../controller/article')
+const {newArticle,getArticleList,getArticleDetail,deleteArticle,updateArticle}=require('../controller/article')
 
 router.prefix('/api/article')
 
@@ -25,5 +25,15 @@ router.get('/getArticleDetail',async function (ctx,next){
     const id=ctx.query.id
     ctx.body=await getArticleDetail(id)
 })
+
+router.post('/deleteArticle',async function (ctx,next){
+    const body=ctx.request.body
+    ctx.body=await deleteArticle(body)
+})
+
+router.post('/updateArticle'),async function (ctx,next){
+    const body=ctx.request.body
+    ctx.body=await updateArticle(body)
+}
 
 module.exports = router
